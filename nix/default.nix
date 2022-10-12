f8da0b76c90811216fdf35a34984dba2eb818231
Peter Hansson <peter.hansson17@gmail.com>
2022-10-12 23:40:57 +0200

    add packages dhall, manix and statix to nix packages

--- nix/default.nix
+++ nix/default.nix
@@ -5,14 +5,17 @@ with pkgs;
 with builtins;
 [
   cowsay
+  dhall
   fortune
   fd
   hadolint
+  html-tidy
   jq
   lolcat
+  manix
   nixfmt
   shellcheck
-  html-tidy
+  statix
   yj
   yq-go
   (import ./fortune-cow.nix { inherit pkgs; })
