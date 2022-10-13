f41c7e9d3f1c7df9d3fb23bfc9882381a3a90b1c
Peter Hansson <peter.hansson17@gmail.com>
2022-10-13 12:23:16 +0200

    simplified flake.nix and support man pages etc

--- flake.nix
+++ flake.nix
@@ -1,32 +1,14 @@
 {
   description = "Lunkentuss user environment";
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
 
   outputs = { self, nixpkgs }:
-    let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
-    in
-      with pkgs;
-      with lib;
-      with builtins;
-      let
-        packages = import nix/default.nix { inherit pkgs; };
-        metaPackage = runCommand
-          "user-environment"
-          {}
-          (
-            pipe
-              packages
-              [
-                (map (package: filesystem.listFilesRecursive "${package}/bin"))
-                flatten
-                (map (package: ''ln -fs "${package}" "$out/bin/$(basename ${package})"'' + "\n"))
-                concatStrings
-                (cmds: ''mkdir -p "$out/bin"'' + "\n" + cmds)
-              ]
-          );
-      in
-        {
-          defaultPackage.x86_64-linux = metaPackage;
-        };
+    with import nixpkgs { system = "x86_64-linux"; };
+    let packages = import nix/default.nix { inherit pkgs; };
+    in {
+      defaultPackage.x86_64-linux = buildPackages.buildEnv {
+        name = "user-environment";
+        paths = packages;
+      };
+    };
 }
