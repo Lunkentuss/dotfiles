ac446225913a830a3cfb3e3823c2e677ae5c7918
Peter Hansson <peter.hansson17@gmail.com>
2022-10-25 18:58:10 +0200

    add comment regarding custom krew packaged

--- nix/default.nix
+++ nix/default.nix
@@ -18,6 +18,8 @@ with builtins; [
   statix
   yj
   yq-go
+  # This package makes running "kubectl krew" work, instead of having to run
+  # krew directly.
   (runCommand "kubectl-krew" { } ''
     mkdir -p "$out/bin" && ln -sf "${krew}/bin/krew" "$out/bin/kubectl-krew"'')
   (import ./fortune-cow.nix { inherit pkgs; })
