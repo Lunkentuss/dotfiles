df8fda42738364eaa2c0563a4a686729aa9d9e9e
Peter Hansson <peter.hansson@jeppesen.com>
2022-11-22 23:38:12 +0100

    add support for other architectures than x86_64 in nix

--- flake.nix
+++ flake.nix
@@ -1,14 +1,17 @@
 {
   description = "Lunkentuss user environment";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }:
-    with import nixpkgs { system = "x86_64-linux"; };
+  outputs = { self, nixpkgs, flake-utils }:
+  flake-utils.lib.eachDefaultSystem (system:
+    with import nixpkgs { inherit system; };
     let packages = import nix/default.nix { inherit pkgs; };
     in {
-      defaultPackage.x86_64-linux = buildPackages.buildEnv {
+      defaultPackage = buildPackages.buildEnv {
         name = "lunkentuss-user-environment";
         paths = packages;
       };
-    };
+    }
+  );
 }
