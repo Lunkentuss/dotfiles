51b1d4d46ffd5230899d746f5856e8ae988e6a71
Peter Hansson <peter.hansson@jeppesen.com>
2022-12-18 02:27:24 +0100

    add nvim completion with lsp integration

--- dotconf/.config/nvim/init.lua
+++ dotconf/.config/nvim/init.lua
@@ -158,3 +158,21 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   }
 end
+
+local cmp = require'cmp'
+cmp.setup({
+  window = {
+    completion = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'buffer' },
+  })
+})
