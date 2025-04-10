7b5254be16884865e05bedd9749fc7ca9b0c4bcd
Peter Hansson <peter.hansson17@gmail.com>
2025-04-10 10:26:21 +0200

    feat: add experimental features to live iso installer

--- nixos/hosts/live-iso/configuration.nix
+++ nixos/hosts/live-iso/configuration.nix
@@ -2,6 +2,9 @@
 with pkgs; {
   networking.networkmanager.enable = true;
   networking.wireless.enable = false;
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   environment.systemPackages = [
     (writeShellScriptBin "nixos-bootstrap" (rootDir + "/nixos-bootstrap"))
     git
