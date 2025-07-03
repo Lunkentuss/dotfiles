992c3a069f76b0816b5798d8dca8fc8ccb47d593
Peter Hansson <poeter.hansson17@gmail.com>
2025-07-03 11:17:35 +0200

    feat: add newest test release of jronnet with overrideAttrs

--- nix/default.nix
+++ nix/default.nix
@@ -80,6 +80,36 @@ in with builtins; [
   jinja2-cli
   jq
   jsonnet
+  (
+    jrsonnet.overrideAttrs (
+      old: rec {
+        cargoBuildFlags = old.cargoBuildFlags or [] ++ [ "--features=exp-preserve-order"  ];
+        version = "v0.5.0-pre96-test";
+        src = fetchFromGitHub {
+          rev = "v0.5.0-pre96-test";
+          owner = "CertainLach";
+          repo = "jrsonnet";
+          sha256 = "sha256-dm62UkL8lbvU3Ftjj6K5ziZGuHpFyLUzyTg9x/+no54=";
+        };
+        cargoDeps = rustPlatform.fetchCargoVendor {
+          inherit src;
+          hash = "sha256-ZHmdlqakucapzXJz6L7ZJpmvqTutelN8qkWAD4uDJr8=";
+        };
+        # Newer version is using generate cmd instead of --generate fla
+        # to geneerate shell completion.
+        postInstall = ''
+          ln -s $out/bin/jrsonnet $out/bin/jsonnet
+        ''
+        + lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
+        for shell in bash zsh fish; do
+          installShellCompletion --cmd jrsonnet \
+            --$shell <($out/bin/jrsonnet generate $shell)
+          installShellCompletion --cmd jsonnet \
+            --$shell <($out/bin/jrsonnet generate $shell | sed s/jrsonnet/jsonnet/g)
+        done
+        '';
+      })
+  )
   k6
   k9s
   kazam
