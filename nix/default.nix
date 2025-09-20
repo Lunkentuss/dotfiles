7b4b182b7a89037a70a45ba3798b3555bb15d206
Peter Hansson <peter.hansson17@gmail.com>
2025-09-20 20:52:06 +0200

    dev: refactor jrsonnet into seperate file

--- nix/default.nix
+++ nix/default.nix
@@ -80,36 +80,6 @@ in with builtins; [
   jinja2-cli
   jq
   jsonnet
-  (
-    jrsonnet.overrideAttrs (
-      old: rec {
-        cargoBuildFlags = old.cargoBuildFlags or [] ++ [ "--features=exp-preserve-order"  ];
-        version = "v0.5.0-pre96-test";
-        src = fetchFromGitHub {
-          rev = "v0.5.0-pre96-test";
-          owner = "CertainLach";
-          repo = "jrsonnet";
-          sha256 = "sha256-dm62UkL8lbvU3Ftjj6K5ziZGuHpFyLUzyTg9x/+no54=";
-        };
-        cargoDeps = rustPlatform.fetchCargoVendor {
-          inherit src;
-          hash = "sha256-ZHmdlqakucapzXJz6L7ZJpmvqTutelN8qkWAD4uDJr8=";
-        };
-        # Newer version is using generate cmd instead of --generate fla
-        # to geneerate shell completion.
-        postInstall = ''
-          ln -s $out/bin/jrsonnet $out/bin/jsonnet
-        ''
-        + lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
-        for shell in bash zsh fish; do
-          installShellCompletion --cmd jrsonnet \
-            --$shell <($out/bin/jrsonnet generate $shell)
-          installShellCompletion --cmd jsonnet \
-            --$shell <($out/bin/jrsonnet generate $shell | sed s/jrsonnet/jsonnet/g)
-        done
-        '';
-      })
-  )
   k6
   k9s
   kazam
@@ -222,6 +192,7 @@ in with builtins; [
     mkdir -p "$out/bin" && ln -sf "${krew}/bin/krew" "$out/bin/kubectl-krew"'')
   (import ./fortune-cow.nix { inherit pkgs; })
   (import ./jksutil.nix { inherit pkgs; })
+  (import ./jrsonnet.nix { inherit pkgs; })
   (import ./kubectl-ctx.nix { inherit pkgs; })
   (import ./kubectl-ns.nix { inherit pkgs; })
 ]
