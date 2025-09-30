81fc5386a7d417d433fed2009ce2ee07cf22074d
Peter Hansson <peter.hansson17@gmail.com>
2025-09-30 20:34:50 +0200

    feat: prepare tfctl for nixpkgs

--- nix/tfctl.nix
+++ nix/tfctl.nix
@@ -9,7 +9,30 @@ buildGoModule rec {
     rev = "v${version}";
     sha256 = "rA3HLO4sD8UmcebGGFxyg0zFpDHCzFHjHwMxPw8MiRU=";
   };
-  subPackages = ["cmd/tfctl"];
   vendorHash = "sha256-NhXgWuxSuurP46DBWOviFzCINJKaTb1mINRYeYcnnH8=";
-  env.CGO_ENABLED = 0;
+
+  subPackages = ["cmd/tfctl"];
+  ldflags = [
+    "-X main.BuildSHA=${src.rev}"
+    "-X main.BuildVersion=${version}"
+  ];
+
+  nativeBuildInputs = [ installShellFiles ];
+  postInstall = lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
+    for shell in bash zsh fish ; do
+      $out/bin/tfctl completion "$shell" > "tfctl.$shell"
+    done
+
+    installShellCompletion tfctl.{bash,zsh,fish}
+  '';
+
+  meta = with lib; {
+    homepage = "https://github.com/flux-iac/tofu-controller";
+    description = "Cli for managing tofu-controller";
+    mainProgram = "tfctl";
+    license = licenses.asl20;
+    maintainers = with maintainers; [
+      lunkentuss
+    ];
+  };
 }
