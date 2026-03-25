6bf2be489e03c0e8c518159ba3fdc1cc38e24fd4
Peter Hansson <peter.hansson17@gmail.com>
2026-03-25 15:26:21 +0100

    feat(jepp): hard code /etc/resolve.conf to fix openvpn domain lookup

--- nix/default.nix
+++ nix/default.nix
@@ -83,6 +83,7 @@ in with builtins; [
   jsonnet
   k6
   k9s
+  keepassxc
   kazam
   kotlin
   krew
@@ -121,7 +122,7 @@ in with builtins; [
   openssh
   openssl
   opentofu
-  openvpn3
+  openvpn
   (python3.withPackages pythonPackages)
   pandoc
   parallel
