18cfff29b2de04b6ae8991f7924ecd1caffffcf1
Peter Hansson <peter.hansson17@gmail.com>
2026-08-08 21:09:47 +0200

    feat: Set PKG_CONFIG_PATH for non nix based projects development

--- nixos/modules/desktop/configuration.nix
+++ nixos/modules/desktop/configuration.nix
@@ -374,7 +374,27 @@ in {
     image = rootDir + "/images/nixos.png";
   };
 
-  environment.sessionVariables = { "EDITOR" = "nvim"; };
+  environment = {
+    sessionVariables = {
+      "EDITOR" = "nvim";
+      "HELLO" = "DOOD";
+      "PKG_CONFIG_PATH" = builtins.concatStringsSep ":" [
+        "${pkgs.openssl.dev}/lib/pkgconfig"
+        "${pkgs.zlib.dev}/share/pkgconfig"
+        "${pkgs.dbus.dev}/lib/pkgconfig"
+        "${pkgs.atkmm.dev}/lib/pkgconfig"
+        "${pkgs.atk.dev}/lib/pkgconfig"
+        "${pkgs.glib.dev}/lib/pkgconfig"
+        "${pkgs.pango.dev}/lib/pkgconfig"
+        "${pkgs.cairo.dev}/lib/pkgconfig"
+        "${pkgs.harfbuzz.dev}/lib/pkgconfig"
+        "${pkgs.gdk-pixbuf.dev}/lib/pkgconfig"
+        "${pkgs.gtk3.dev}/lib/pkgconfig"
+        "${pkgs.webkitgtk_4_1.dev}/lib/pkgconfig"
+        "${pkgs.libsoup_3.dev}/lib/pkgconfig"
+      ];
+    };
+  };
 
   system.activationScripts.binbash = {
     text = ''
